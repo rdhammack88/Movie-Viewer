3d1b18e2b0dea0b459a37259250d16fbffdaf28b
Dustin Hammack <rdhammack88@hotmail.com>
2018-05-03 00:04:32 -0500

    API call to DB working with search. Also added click of search movie button to get API info, as well added on press of Enter key in search form to retrieve movie info and display the returned movies

--- js/controllers.js
+++ js/controllers.js
@@ -2,20 +2,38 @@ var movieControllers = angular.module('movieControllers', []);
 
 movieControllers.controller('MovieSearchController', 
 	function MovieController($scope, $http) {
-//		if($scope.movieQuery) {
+
+		$scope.findMovie = function() {
 			$http({
 				method: 'GET',
 				url: 'http://www.omdbapi.com/?apikey=a336cd51&s=' + $scope.movieQuery
 			}).then(function(res) {
 				$scope.movies = res.data.Search;
-	//			$scope.movie = res.data;
 				console.log($scope.movies);
 				console.log($scope.movieQuery);
 	//			$scope.movieID = movie.imdbID.toString();
 			});
-//		}
+		}
+		
+//		document.querySelector('#searchText').on('keypress', function(e) {
+//			if(e.keyCode === 13) {
+//				$('.save-list').click();
+//			}
+//		});
 	}
 );
+
+movieControllers.directive('enterClick', function() {
+	return function(scope, element) {
+		element.bind('keypress', function(e) {
+			if(e.keyCode === 13) {
+				scope.$apply(function() {
+					$scope.findMovie();
+				})
+			}
+		})
+	}
+})
 
 movieControllers.controller('MovieDetailsController', 
 	function MovieController($scope, $http) {
