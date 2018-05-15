d4aae1698b2d85607f31c101ebc1eeae48c0326d
Dustin Hammack <rdhammack88@hotmail.com>
2018-05-15 02:59:07 -0500

    Testing display of movies with no images

--- index.php
+++ index.php
@@ -11,14 +11,18 @@
 	<script src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
 	<link rel="stylesheet" href="css/styles.css">
 	
+<!--
 	<script src="lib/angular/angular.min.js"></script>
 	<script src="lib/angular/angular-route.min.js"></script>
+-->
+<script data-require="angular.js@1.3.0-beta.5" data-semver="1.3.0-beta.5" src="https://code.angularjs.org/1.3.0-beta.5/angular.js"></script>
+    <script data-require="angular-route@1.2.14" data-semver="1.2.14" src="https://code.angularjs.org/1.2.14/angular-route.js"></script>
+    
 	<script src="js/app.js"></script>
-<!--
 	<script src="js/SearchController.js"></script>
 	<script src="js/DetailsController.js"></script>
--->
-	<script src="js/controllers.js"></script>
+	<script src="js/omdb.js"></script>
+<!--	<script src="js/controllers.js"></script>-->
 </head>
 <body ng-view>
 	
