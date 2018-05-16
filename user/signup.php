551dd8740e1985a26346ad02a3edc9081b48926d
Dustin Hammack <rdhammack88@hotmail.com>
2018-05-16 00:13:58 -0500

    Signup and Login pages coded, but not complete

---
+++ user/signup.php
@@ -0,0 +1,26 @@
+<div class="container-fluid">
+	<form>
+		<fieldset>
+			<div class="form-group">
+				<label for="InputName">Name</label>
+				<input type="email" class="form-control" id="InputName" placeholder="Enter name">
+			</div>
+			<div class="form-group">
+				<label for="InputEmail1">Email address</label>
+				<input type="email" class="form-control" id="InputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+				<small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+			</div>
+			<div class="form-group">
+				<label for="InputPassword1">Password</label>
+				<input type="password" class="form-control" id="InputPassword1" placeholder="Password">
+			</div>			
+			<div class="form-group">
+				<label for="RetypeInputPassword1">Retype Password</label>
+				<input type="password" class="form-control" id="RetypeInputPassword1" placeholder="Retype Password">
+			</div>
+			<div class="form-group">
+				<input type="submit" name="signup" value="Signup" class="btn btn-success">
+			</div>
+		</fieldset>
+	</form>
+</div>
