245510e8b0ca0f5ec6b4d7c4f95be618f09f104a
Dustin Hammack <rdhammack88@hotmail.com>
2018-06-01 23:53:36 -0500

    Added ng-submit to form, and added name attributes to input elements

--- js/partials/user/signup.php
+++ js/partials/user/signup.php
@@ -1,22 +1,22 @@
 <div class="container-fluid">
-	<form>
+	<form ng-submit="userSignup()">
 		<fieldset>
 			<div class="form-group">
 				<label for="InputName">Name</label>
-				<input type="email" class="form-control" id="InputName" placeholder="Enter name">
+				<input type="email" class="form-control" id="InputName" placeholder="Enter name" value="{{userName}}" name="user_name">
 			</div>
 			<div class="form-group">
 				<label for="InputEmail1">Email address</label>
-				<input type="email" class="form-control" id="InputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+				<input type="email" class="form-control" id="InputEmail1" aria-describedby="emailHelp" placeholder="Enter email" value="{{userEmail}}" name="user_email">
 				<small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
 			</div>
 			<div class="form-group">
 				<label for="InputPassword1">Password</label>
-				<input type="password" class="form-control" id="InputPassword1" placeholder="Password">
+				<input type="password" class="form-control" id="InputPassword1" placeholder="Password" value="{{userPw}}" name="user_pw">
 			</div>			
 			<div class="form-group">
 				<label for="RetypeInputPassword1">Retype Password</label>
-				<input type="password" class="form-control" id="RetypeInputPassword1" placeholder="Retype Password">
+				<input type="password" class="form-control" id="RetypeInputPassword1" placeholder="Retype Password" value="{{userPwRepeat}}" name="user_pw_repeat">
 			</div>
 			<div class="form-group">
 				<input type="submit" name="signup" value="Signup" class="btn btn-success">
