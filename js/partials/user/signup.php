17470865ee4e791d01e7c73238980ab731963c33
Dustin Hammack <rdhammack88@hotmail.com>
2018-06-02 23:59:37 -0500

    Added ng-model to input fields

--- js/partials/user/signup.php
+++ js/partials/user/signup.php
@@ -3,20 +3,20 @@
 		<fieldset>
 			<div class="form-group">
 				<label for="InputName">Name</label>
-				<input type="email" class="form-control" id="InputName" placeholder="Enter name" value="{{userName}}" name="user_name">
+				<input type="text" class="form-control" id="InputName" placeholder="Enter name" ng-model="userName" name="user_name">
 			</div>
 			<div class="form-group">
 				<label for="InputEmail1">Email address</label>
-				<input type="email" class="form-control" id="InputEmail1" aria-describedby="emailHelp" placeholder="Enter email" value="{{userEmail}}" name="user_email">
+				<input type="email" class="form-control" id="InputEmail1" aria-describedby="emailHelp" placeholder="Enter email" ng-model="userEmail" value="{{userEmail}}" name="user_email">
 				<small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
 			</div>
 			<div class="form-group">
 				<label for="InputPassword1">Password</label>
-				<input type="password" class="form-control" id="InputPassword1" placeholder="Password" value="{{userPw}}" name="user_pw">
+				<input type="password" class="form-control" id="InputPassword1" placeholder="Password" ng-model="userPassword" value="{{userPw}}" name="user_pw">
 			</div>			
 			<div class="form-group">
 				<label for="RetypeInputPassword1">Retype Password</label>
-				<input type="password" class="form-control" id="RetypeInputPassword1" placeholder="Retype Password" value="{{userPwRepeat}}" name="user_pw_repeat">
+				<input type="password" class="form-control" id="RetypeInputPassword1" placeholder="Retype Password" ng-model="userPasswordRepeat" value="{{userPwRepeat}}" name="user_pw_repeat">
 			</div>
 			<div class="form-group">
 				<input type="submit" name="signup" value="Signup" class="btn btn-success">
