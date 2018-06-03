9af0ddcc5a7aa397327e629319f1c869756336b9
Dustin Hammack <rdhammack88@hotmail.com>
2018-06-03 00:00:16 -0500

    Added tests for $http service

--- js/SignupController.js
+++ js/SignupController.js
@@ -6,12 +6,44 @@
 		$scope.userEmail;
 		$scope.userPw;
 		$scope.userPwRepeat;
+//		{name: name}
+		
 		
 		$scope.userSignup = function() {
-			$http.get('./includes/admin/user_signup.php')
-				.then(function(res) {
-					console.log( res.data );
-				});
+			
+			console.log($scope.userName);
+//			$http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
+//			$http.post('./includes/admin/user_signup.php', {'user_name': $scope.userName})
+//				.then(function(res) {
+//					console.log( res.data );
+////					console.log(name);
+//				});
+			
+//			{user_name: $scope.userName},
+//			'user_name=' + $scope.userName + '&user_email=' + $scope.userEmail,
+			
+//			let user_name = $scope.userName;
+//			let user_email = $scope.userEmail;
+			var qdata = {user_name: 'user_name'};
+			//, user_email: 'user_email'};
+			
+//			$http.post('./includes/admin/user_signup.php', qdata)
+//				.then(function(data, status, headers, config) {
+//					console.log( headers );
+////					console.log(name);
+//				});
+			
+			$http({
+				method: 'POST',
+				url: './includes/admin/user_signup.php',
+//				dataType: 'json',
+				data: qdata,
+				headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+			})
+			.then(function(res) {
+				console.log( res.data );
+//					console.log(name);
+			});
 		}
 		
 	}
